7d2c11720d2684befc10b58f53426c45e7c4d878
Krishnendu Mukherjee <96240550+kishnendu@users.noreply.github.com>
2023-08-31 00:12:59 +0530

    Update HR Data Cleaning.sql

--- HR Data Cleaning.sql
+++ HR Data Cleaning.sql
@@ -2,57 +2,57 @@ CREATE DATABASE projects;
 
 USE projects;
 
-SELECT * FROM hr;
+SELECT * FROM hr_data;
 
-ALTER TABLE hr
+ALTER TABLE hr_data
 CHANGE COLUMN ï»¿id emp_id VARCHAR(20) NULL;
 
-DESCRIBE hr;
+DESCRIBE hr_data;
 
-SELECT birthdate FROM hr;
+SELECT birthdate FROM hr_data;
 
 SET sql_safe_updates = 0;
 
-UPDATE hr
+UPDATE hr_data
 SET birthdate = CASE
 	WHEN birthdate LIKE '%/%' THEN date_format(str_to_date(birthdate, '%m/%d/%Y'), '%Y-%m-%d')
     WHEN birthdate LIKE '%-%' THEN date_format(str_to_date(birthdate, '%m-%d-%Y'), '%Y-%m-%d')
     ELSE NULL
 END;
 
-ALTER TABLE hr
+ALTER TABLE hr_data
 MODIFY COLUMN birthdate DATE;
 
-UPDATE hr
+UPDATE hr_data
 SET hire_date = CASE
 	WHEN hire_date LIKE '%/%' THEN date_format(str_to_date(hire_date, '%m/%d/%Y'), '%Y-%m-%d')
     WHEN hire_date LIKE '%-%' THEN date_format(str_to_date(hire_date, '%m-%d-%Y'), '%Y-%m-%d')
     ELSE NULL
 END;
 
-ALTER TABLE hr
+ALTER TABLE hr_data
 MODIFY COLUMN hire_date DATE;
 
-UPDATE hr
+UPDATE hr_data
 SET termdate = date(str_to_date(termdate, '%Y-%m-%d %H:%i:%s UTC'))
 WHERE termdate IS NOT NULL AND termdate != ' ';
 
-ALTER TABLE hr
+ALTER TABLE hr_data
 MODIFY COLUMN termdate DATE;
 
 ALTER TABLE hr ADD COLUMN age INT;
 
-UPDATE hr
+UPDATE hr_data
 SET age = timestampdiff(YEAR, birthdate, CURDATE());
 
 SELECT 
 	min(age) AS youngest,
     max(age) AS oldest
-FROM hr;
+FROM hr_data;
 
-SELECT count(*) FROM hr WHERE age < 18;
+SELECT count(*) FROM hr_data WHERE age < 18;
 
-SELECT COUNT(*) FROM hr WHERE termdate > CURDATE();
+SELECT COUNT(*) FROM hr_data WHERE termdate > CURDATE();
 
 SELECT COUNT(*)
 FROM hr
